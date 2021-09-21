e16493a83cd2f61a808bb365563c61c7b836944d
Teamer Tibebu <teamer.tibebu@revelry.co>
2021-09-21 15:02:28 -0500

    (add) form for new topics and route to capture form data.

--- lib/haha_web/controllers/topic_controller.ex
+++ lib/haha_web/controllers/topic_controller.ex
@@ -1,11 +1,17 @@
 defmodule HahaWeb.TopicController do
   use HahaWeb, :controller
+  alias HahaWeb.Topic
 
   # def index(conn, _params) do
   #   render(conn, "index.html")
   # end
 
   def new(conn, _params) do
-    render(conn, "index.html")
+    changeset = Topic.changeset(%Topic{}, %{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(_conn, %{"topic" => topic}) do
+    IO.inspect(topic)
   end
 end
--- lib/haha_web/router.ex
+++ lib/haha_web/router.ex
@@ -18,6 +18,7 @@ defmodule HahaWeb.Router do
 
     get "/", PageController, :index
     get "/topics/new", TopicController, :new
+    post "/topics", TopicController, :create
   end
 
   # Other scopes may use custom stacks.
