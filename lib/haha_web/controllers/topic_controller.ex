97432f04fcedb59a487125c3576aa0b044da412b
Teamer Tibebu <teamer.tibebu@revelry.co>
2021-09-21 21:51:28 -0500

    (add) update & edit functions in Topic controller to update topic names

--- lib/haha_web/controllers/topic_controller.ex
+++ lib/haha_web/controllers/topic_controller.ex
@@ -5,6 +5,7 @@ defmodule HahaWeb.TopicController do
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
+
     render(conn, "index.html", topics: topics)
   end
 
@@ -17,13 +18,33 @@ defmodule HahaWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, _post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: Routes.topic_path(conn, :index))
 
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+    changeset = Topic.changeset(topic, %{})
+
+    render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic_changes}) do
+    old_topic = Repo.get!(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic_changes)
+
+    case Repo.update(changeset) do
+      {:ok, topic} ->
+        conn
+        |> put_flash(:info, "Topic Edited")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} -> IO.inspect(changeset)
     end
   end
 end
--- lib/haha_web/router.ex
+++ lib/haha_web/router.ex
@@ -19,6 +19,8 @@ defmodule HahaWeb.Router do
     get "/", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id", TopicController, :update
   end
 
   # Other scopes may use custom stacks.
